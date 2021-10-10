91660bcf752938783ebf5968e8e34b4d2e7bc6f3
llwafelll <motorolamarcin@gmail.com>
2021-10-11 00:46:52 +0200

    Modified moj_program.c as described in instrudtion.

--- PR_lab/lab_1/moj_program.c
+++ PR_lab/lab_1/moj_program.c
@@ -1,6 +1,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<time.h>
+#include<pomiar_czasu.h>
  
 const int liczba = 100000;
 
@@ -10,17 +11,22 @@ main(){
   int i,j,k,l;
 
   k = -50000;
+
+	inicjalizuj_czas()	
   for(i=0;i<liczba;i++){
 
     printf("%d ",k+i);
 
   }
   printf("\n");
-
   printf("Czas wykonania %d operacji wejscia/wyjscia: \n",liczba);
+	drukuj_czas();
 
 
   a = 1.000001;
+
+	t1 = czas_zegara();
+	t2 = czas_CPU();
   for(i=0;i<liczba;i++){
 
     a = 1.000001*a+0.000001; 
@@ -29,6 +35,9 @@ main(){
 
   printf("Wynik operacji arytmetycznych: %lf\n", a);
   printf("Czas wykonania %d operacji arytmetycznych: \n",liczba);
+
+	t1 = czas_zegara() - t1;
+	t2 = czas_CPU() - t2;
 
 
 }
