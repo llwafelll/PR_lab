5eaf8934f35ebadb58eadbc5207dac92ae8d7aa2
llwafelll <motorolamarcin@gmail.com>
2021-10-11 10:48:06 +0200

    Made Makefile work, added comments.

--- PR_lab/lab_1/moj_program.c
+++ PR_lab/lab_1/moj_program.c
@@ -5,14 +5,21 @@
  
 const int liczba = 100000;
 
-main(){
+int main(){
+
+	// Deklaracja zmiennych do zapisu czasu
+	double t1, t2;
 
   double a, b, c;
   int i,j,k,l;
 
+	// PETLA Z OPERACJAMI WEJSCIA/WYJSCIA
+	
   k = -50000;
 
-	inicjalizuj_czas()	
+	// Rozpoczecie pomiaru czasu dla petli z operacjami we/wy
+	inicjuj_czas();
+
   for(i=0;i<liczba;i++){
 
     printf("%d ",k+i);
@@ -20,13 +27,19 @@ main(){
   }
   printf("\n");
   printf("Czas wykonania %d operacji wejscia/wyjscia: \n",liczba);
+
+	// Wypisanie czasu na ekran
 	drukuj_czas();
 
 
+	// PETLA Z OPERACJAMI ARYTMETYCZNYMI
+	
   a = 1.000001;
 
+	// Rozpoczecie pomiaru czasu dla petli z operacjami arytmetycznymi
 	t1 = czas_zegara();
 	t2 = czas_CPU();
+
   for(i=0;i<liczba;i++){
 
     a = 1.000001*a+0.000001; 
@@ -36,8 +49,13 @@ main(){
   printf("Wynik operacji arytmetycznych: %lf\n", a);
   printf("Czas wykonania %d operacji arytmetycznych: \n",liczba);
 
+	// Zakonczenie pomiaru czasu dla petli z operacjami arytmetycznymi
 	t1 = czas_zegara() - t1;
 	t2 = czas_CPU() - t2;
+
+	// Wypisanie czasu na ekran
+	printf("czas CPU      = %lf\n", t2);
+	printf("czas zegarowy = %lf\n", t1);
 
 
 }
